471e2dee7575b444a68b414de8127ee56473e012
Rae Tiffen <rae@raetiffen.com>
2025-11-23 16:08:17 -0500

    upd

--- OLS algorithms/recursive block inverse.py
+++ OLS algorithms/recursive block inverse.py
@@ -2,43 +2,34 @@
     Rae Tiffen
 
     No idea if this will work, but might as well try
+
+    Note: The input notation for this is different than my other OLS algorithms. X[i][j] is the element in the ith row and jth column of X. That is, X is a list of lists, where each list is a row (NOT a column). Below I use the same y and X as the other algorithms (where each list is a column/variable), I simply transpose them before inputting it into the OLS function.
 """
 
-"""
-    notes for writing:
-
-
-    a row vector is a list
-    a matrix is a list of row vectors
-    so a column vector would be a list of single-entry lists
-    if X is a matrix, X[i][j] gets the element in the ith row and jth column
-    why am I not using numpy? because I'm trying to make this w/o any packages
-"""
-
-col = [[1], [2], [3], [4], [5]]
-row = [[6, 7, 8, 9, 10]]
-matA = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
 """
     let A be an m by n matrix
     len(A) is the number of rows
     len(A[0]) is the number of columns, assuming all rows have same number of entries (it must)
-
-    # A m by n. m rows and n columns.
-    # B n by p. n rows and p columns
-    # Verify conformable?
-    # For each row in the output. Then for each column in the output
-    # There is a row in the output for each row in A
-    # There is a column in the output for each column in B
-    # Each entry is the dot product of row i of A with column j of B
+    A[i][j] gets the element in the ith row and jth column
 """
+
+y = [[1,2,5,3,7,2,9]]
+X = [[1,1,1,1,1,1,1],[1,2,3,4,5,6,7],[4,3,2,1,0,9,8],[6,2,5,8,9,2,5],[8,5,2,7,4,2,5]]
 
 def matmult(A,B):
     return [[sum(A[i][k] * B[k][j] for k in range(len(A[0]))) for j in range(len(B[0]))] for i in range(len(A))]
 
-
 def transpose(A):
     return [[A[i][j] for i in range(len(A))] for j in range(len(A[0]))]
 
+def ols(y,X):
+    XtX = matmult(transpose(X),X)
+    Xty = matmult(transpose(X),y)
+    beta = matmult(block_inverse(XtX),Xty)
+    return beta
 
-print(matmult(matA,col))
-print(transpose(matA))
+def block_inverse(A):
+    return
+
+if __name__== "__main__":
+    print(ols(transpose(y),transpose(X)))
