e8cb0bd1b2c84ed3d5d4e7d16ba34bdcc8e147ff
Rae Tiffen <rae@raetiffen.com>
2025-11-23 15:55:08 -0500

    transpose function

--- OLS algorithms/recursive block inverse.py
+++ OLS algorithms/recursive block inverse.py
@@ -33,15 +33,12 @@ matA = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
 """
 
 def matmult(A,B):
-    if len(A[0]) != len(B):
-        print("Not conformable")
-        return
     return [[sum(A[i][k] * B[k][j] for k in range(len(A[0]))) for j in range(len(B[0]))] for i in range(len(A))]
-    """    AB = []
-    for i in range(len(A)):
-        AB.append([])
-        for j in range(len(B[0])):
-            AB[i].append(sum(A[i][k] * B[k][j] for k in range(len(A[0]))))"""
-    return AB
+
+
+def transpose(A):
+    return [[A[i][j] for i in range(len(A))] for j in range(len(A[0]))]
+
 
 print(matmult(matA,col))
+print(transpose(matA))
